9f66026e766a7aa04a03c7106516e8db99280cd3
jawad qasim <jawadqasim99@gmail.com>
2023-02-13 20:20:39 +0500

    sylani assignent javascript 1st

---
+++ index.js
@@ -0,0 +1,117 @@
+// 1.	Declare two variables: admin and name. Assign the value "Umair Azmat" to name. Copy the value from name to admin. Show the value of admin using alert (must output Name).
+
+// var admin ;
+// var name = "Umair Azmat";
+
+// admin = name;
+// alert( "Admin:"+" " + admin);
+
+
+// Create a variable with the name of our planet. How would you name such a variable?
+
+// var country = "EARTH";
+
+
+// console.log(country);
+
+
+// 3.	Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+
+// var VisterName= "Umair Azmat";
+// alert("Vister :" + VisterName);
+
+
+// 4.	Create a string variable called favDrink and assign a value to it.
+// var favDrink = "coke";
+// console.log(favDrink);
+
+// 5.	Create a number variable called favNum and assign a value to it
+// var favNum = 89;
+// console.log(favNum);
+
+// 6.	Create a function block called myFavs that console logs your answers, don't forget to invoke the myFavs function.
+
+// function myFavs() {
+//     var myFav="Azmat umair";
+//     console.log(myFav);
+
+// }
+// myFavs();
+
+// 7.	Create an object called person, with name set to Umair and age set to 20.
+
+// var person = {
+//     name : "umair",
+//     age : 20
+// }
+// console.log(person);
+
+
+// 8.	write a JS program, define two variables and sum those two variable and save result in third variable and print in console?
+
+// var a=7;
+// var b=23;
+// c=a+b;
+// console.log(c);
+
+// 9.	write a JS program, that can divide two numbers and show the result in console? console.log("result of firstProgram", a);
+
+// var a = 3;
+// var b=8;
+// c=a/b;
+// console.log("Result of the firstprogram",c);
+
+// 10.	write a JS program, that can add two number, divide result by 5 and show in console?console.log("result of firstProgram", a);
+
+// a=25;
+// b=5;
+// c=a/b;
+// console.log("Result of the firstprogram",c);
+
+// 11.	write a JS program, that can convert temp from F into C and show into console, using C = (F − 32) * 5/9. use if F = 50, what will be the value of C? console.log("Divide two number", a);
+
+// var F=50;
+// c= (F-32)*5/9
+// console.log(c);
+
+
+// 12.	Write a JavaScript program to find the sum of two numbers obtained from prompt and display the result using alert.
+
+// var num1 = parseFloat(prompt("Enter the first number:"));
+// var num2 = parseFloat(prompt("Enter the second number:"));
+// var sum = num1 + num2;
+// alert("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+
+// 13.	Write a JavaScript program to find the difference of two numbers obtained from prompt and display the result using console.log.
+
+// var num1 = parseFloat(prompt("Enter the first number:"));
+// var num2 = parseFloat(prompt("Enter the second number:"));
+// var sum = num1 - num2;
+// alert("The difference of " + num1 + " and " + num2 + " is " + sum);
+
+// 14.	Write a JavaScript program to find the product of two numbers obtained from prompt and display the result using alert.
+
+
+
+// var num1 = parseFloat(prompt("Enter the first number:"));
+// var num2 = parseFloat(prompt("Enter the second number:"));
+// var sum = num1 * num2;
+// alert("The product of " + num1 + " and " + num2 + " is " + sum);
+
+
+// 15.	Write a JavaScript program to find the quotient of two numbers obtained from prompt and display the result using console.log.
+
+// var num1 = parseFloat(prompt("Enter the first number:"));
+// var num2 = parseFloat(prompt("Enter the second number:"));
+// var sum = num1 / num2;
+// alert("The quotient of " + num1 + " and " + num2 + " is " + sum);
+
+
+// 16.	Write a JavaScript program to find the remainder of two numbers obtained from prompt using the modulus operator and display the result using alert.
+
+var num1 = parseFloat(prompt("Enter the first number:"));
+var num2 = parseFloat(prompt("Enter the second number:"));
+var sum = num1 % num2;
+alert("The premainder of " + num1 + " and " + num2 + " is " + sum);
